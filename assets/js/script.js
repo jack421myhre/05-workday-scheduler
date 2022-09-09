235f8b08eaa5d4abc08bfd6df4f2e72f5c13b473
Jackson Myhre <jack421myhre@gmail.com>
2022-09-08 19:57:42 -0500

    Added functionality to the time slots, indicating past, present, and future.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,33 @@
+let saveBtn = $(".saveBtn");
+
 function headerDate() {
     let currentDay = moment().format("MMMM Do YYYY");
     $("#currentDay").text(currentDay);
 }
-
 headerDate();
+let currentTime = moment().format("HH");
+
+let wrapperParent = $(".wrapper");
+// for (let i = 0; i < wrapperParent.children().length; i++) {
+//     let hour = $(`#`);
+//     console.log(hour);
+
+//     if (hour.text() < currentTime) {
+//         console.log("past");
+//     }
+// }
+
+$(".hour").each(function () {
+    let timeBlockHour = parseInt($(this).prop("id"));
+    // console.log(timeBlockHour);
+    if (timeBlockHour < currentTime) {
+        $(this).siblings("textarea").addClass("past");
+        $(this).siblings("textarea").removeClass("present");
+    } else if (timeBlockHour > currentTime) {
+        $(this).siblings("textarea").addClass("future");
+        $(this).siblings("textarea").removeClass("past");
+    } else {
+        $(this).siblings("textarea").addClass("present");
+        $(this).siblings("textarea").removeClass("future");
+    }
+});
