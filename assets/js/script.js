335627e6b34c08c2ba72ab50a77c479ee511d7ba
Jackson Myhre <jack421myhre@gmail.com>
2022-09-10 15:16:15 -0500

    Completed saving and loading local storage to the page. Added a Save icon via bootstrap to each button element.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 let saveBtn = $(".saveBtn");
-
+// Keeps the jumbotron date up to date using moment.js
 function headerDate() {
     let currentDay = moment().format("MMMM Do YYYY");
     $("#currentDay").text(currentDay);
 }
 headerDate();
-let currentTime = moment().format("HH");
-let timeBlockHour = $(".hour").prop("id");
 
+let currentTime = moment().format("HH");
 $(".hour").each(function () {
+    // Grabs the id number from each time block and parses and int from it.
     let timeBlockHour = parseInt($(this).prop("id"));
-    // console.log(timeBlockHour);
+    // Checks current time against each time block and applies the color classes accordingly, removing older ones.
     if (timeBlockHour < currentTime) {
         $(this).siblings("textarea").addClass("past");
         $(this).siblings("textarea").removeClass("present");
@@ -23,21 +23,19 @@ $(".hour").each(function () {
     }
 });
 
-// Save data
-// Work in Progress
-let eventDescription = $(".description");
-function saveEvent() {}
+// Loads the saved tasks in each time block respectively.
+$(".time-block").each(function () {
+    time = $(this).children(".hour").prop("id");
+    let setEvent = window.localStorage.getItem(time);
+    $(this).children("textarea").text(setEvent);
+});
 
+// Saves the tasks to local storage for each time block.
 saveBtn.on("click", () => {
-    if (eventDescription !== "") {
-        let savedEvent = JSON.parse(localStorage.getItem("savedEvent")) || [];
+    $(".time-block").each(function () {
+        time = $(this).children(".hour").prop("id");
+        eventDesc = $(this).children("textarea").val();
 
-        let allEvents = {
-            time: $(this).siblings(".hour").prop("id"),
-            eventDesc: $(this).siblings("textarea").val(),
-        };
-
-        savedEvent.push(allEvents);
-        localStorage.setItem("savedEvent", JSON.stringify(savedEvent));
-    }
+        window.localStorage.setItem(time, eventDesc);
+    });
 });
